ad3bf9d352a1cad3d688c8eea1b89cdf5655b818
AllainDimitris <36037208+AllainDimitris@users.noreply.github.com>
2018-10-04 16:22:29 +0200

    Trigger

---
+++ base/TRIGGER.sql
@@ -0,0 +1,34 @@
+update client set nbreservation=(select count(idr) from reservation where client.idcl=reservation.idcl group by client.idcl);
+
+
+
+drop trigger if exists nbreservation;
+delimiter //
+create trigger nbreservation
+
+after 
+	insert on reservation for each row
+begin
+	update client set nbreservation=nbreservation+1 where idcl=new.idcl;
+end //
+delimiter ;
+
+
+
+drop trigger if exists dropreservation;
+delimiter //
+create trigger dropreservation
+
+after 
+	delete on reservation for each row
+begin
+	update client set nbreservation=nbreservation-1 where idcl=old.idcl;
+end //
+delimiter ;
+
+
+insert into reservation values (1, 1,1, '2018-09-25', '2018-10-15', 'oui', 'été', 15);
+insert into reservation values (3, 1,1, '2018-09-25', '2018-10-15', 'oui', 'été', 36);
+
+
+insert into reservation values (2, 2,1, '2018-09-25', '2018-10-15', 'oui', 'été', 20);
