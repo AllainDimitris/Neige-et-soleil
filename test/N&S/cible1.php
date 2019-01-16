426d782e5a169b76767fae1c839b946eac85be95
ayoub <aybinho@github.com>
2019-01-16 09:45:07 +0100

    nouveau formulaire
    KEVIN TA MERE ARRETE DE TOUT CASSER YEN A MARRE ESPECE D'ENFOIRE

--- test/N&S/cible1.php
+++ test/N&S/cible1.php
@@ -14,12 +14,8 @@ $a=$_POST['cciv'];
 $b=$_POST['cnom'];
 $c=$_POST['cprnom'];
 $d=$_POST['cmail'];
-$e=$_POST['cadr'];
-$f=$_POST['ccp'];
-$g=$_POST['cvil'];
 $h=$_POST['ctel'];
 $i=$_POST['cdate'];
-$j=$_POST['clog'];
 $k=password_hash($_POST['cmdp'], PASSWORD_DEFAULT);
 
 
@@ -32,7 +28,7 @@ try
     // Pourquoi ? ça permet de dire à PDO de générer des exceptions quand il y a des problèmes, sinon il le fait pas et les try...catch ne servent à rien
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} 
+}
 catch (Exeption $erreur)
 {
     die('Erreur :'.$erreur->getMessage());
@@ -52,11 +48,11 @@ try {
 
     // Ca parait plus compliqué mais on s'y fait (et c'est surtout plus sécuritaire)
 
-    $query = $bdd->prepare("INSERT INTO `client`(`SexeCL`, `NOMCL`, `PRENOMCL`, `ADRMAILCL`, `ADRCL`, `CPCL`, `VILLECL`, `TelCL`, `DATENAICL`, `LoginCL`, `MdpCL`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+    $query = $bdd->prepare("INSERT INTO `client`(`SexeCL`, `NOMCL`, `PRENOMCL`, `ADRMAILCL`,`TelCL`, `DATENAICL`,`MdpCL`) VALUES (?,?,?,?,?,?,?)");
 
 
     // "$query->execute" retourne "true" si ça s'est bien passé
-    $isItOk = $query->execute([$a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k]);
+    $isItOk = $query->execute([$a, $b, $c, $d,$h, $i,$k]);
     if($isItOk) {
         echo "Ajout du client effectué" ;
     }
@@ -70,9 +66,8 @@ try {
 
 // Ok pour le "include", ça fonctionnne : mais pourquoi ne pas faire une redirection vers la première page ?
 // header('Location: Questionnaire1.php');
-// Si tu veux afficher un message de confirmation tu peux le passer avec des paramètres GET 
+// Si tu veux afficher un message de confirmation tu peux le passer avec des paramètres GET
 //header('Location: Questionnaire1.php?ajout=ok');
 include("form.php");
 
 ?>
-
