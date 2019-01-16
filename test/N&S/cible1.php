3e5a676860c0c1538a78d540701925dcff6f8971
AllainDimitris <36037208+AllainDimitris@users.noreply.github.com>
2019-01-16 09:27:38 +0100

    a

--- test/N&S/cible1.php
+++ test/N&S/cible1.php
@@ -72,7 +72,7 @@ try {
 // header('Location: Questionnaire1.php');
 // Si tu veux afficher un message de confirmation tu peux le passer avec des paramètres GET 
 //header('Location: Questionnaire1.php?ajout=ok');
-include("form.php");
+include("connexion.php");
 
 ?>
 
