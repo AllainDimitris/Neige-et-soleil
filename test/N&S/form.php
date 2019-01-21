208c03aadde23e27dd76f4bba31e944d5b417833
MonninKevin <36037158+MonninKevin@users.noreply.github.com>
2019-01-21 10:34:35 +0100

    .

--- test/N&S/form.php
+++ test/N&S/form.php
@@ -8,15 +8,15 @@
     <link rel="stylesheet" href="css/form.css"/>
     <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 
-     
+
     </head>
 
 
   <body class="is-preload">
 
-    <div id="wrapper"> 
+    <div id="wrapper">
  	<?php include("header.php"); ?>
-         
+
 
 <?php
 /*
@@ -29,7 +29,7 @@
   }
 */
 ?>
-
+  <br>
   <h1 align="center"> Veuillez entrer vos informations</h1>
   <body bgcolor="#9683EC">
 
@@ -37,32 +37,26 @@
 
 <div id="question">
 	Sexe <br>
-  <input type="radio" name="cciv" id="homme" value="homme">  <label for="homme">Homme</label> 
-  <input type="radio" name="cciv" id="femme" value="femme">  <label for="femme">Femme</label>
+  <select name="cciv">
+  <option name="cciv" id="homme" value="homme">Homme</option>
+  <option name="cciv" id="femme" value="femme">Femme</option>
+  </select>
   <br><br>
 	<label for="nom">Nom</label>
 	<input type="text" name="cnom" maxlength="30" placeholder="Nom" autofocus required pattern="[A-Za-z]{1,30}" title="Caractère spéciaux non admit"><br>
 	<label for="prénom">Prénom</label>
 	<input type="text" name="cprnom" maxlength="30" placeholder="Prénom" required pattern="[A-Za-z]{1,30}" title="Caractère spéciaux non admit"><br>
-	<label for="email">E-Mail</label>
-	<input type="email" name="cmail" maxlength="50" placeholder="E-Mail" required ><br>
-	<label for="adresse">Adresse</label>
-	<input type="text" name="cadr" maxlength="100" placeholder="Adresse" required ><br>
-	<label for="CP">Code Postal</label>
-	<input type="text" name="ccp" maxlength="5" placeholder="Code Postal" required ><br>
- 	<label for="CP">Ville</label>
-	<input type="text" name="cvil" maxlength="30" placeholder="Ville" required ><br>
- 	<label for="Numéro de téléphone">Téléphone</label>
- 	<input type="text" name="ctel"  maxlength="10" placeholder="Téléphone" required><br>
- 	<label for="Date de naissance">Date de naissance</label>
- 	<input type="date" name="cdate"  maxlength="40" placeholder="Date de naissance" required ><br>
- 	<label for="Login">Login</label>
- 	<input type="text" name="clog" maxlength="20" placeholder="Login" required pattern="[A-Za-z]{1,30}" title="Caractère spéciaux non admit"><br>
+  <label for="Date de naissance">Date de naissance</label>
+  <input type="date" name="cdate"  maxlength="40" placeholder="Date de naissance" required><br>
+  <label for="Numéro de téléphone">Téléphone</label>
+  <input type="text" name="ctel"  maxlength="10" placeholder="Téléphone" required><br>
+  <label for="email">E-Mail</label>
+  <input type="email" name="cmail" maxlength="50" placeholder="E-Mail" required ><br>
 	<label for="Mot de passe">Mot de passe</label>
 	<input type="password" name="cmdp"  maxlength="20" placeholder="Mot de passe" required ><br>
 	<center>
 	<label></label>
-	<input id="submit" type="submit" value="Envoyer le formulaire  ">	
+	<input id="submit" type="submit" value="Envoyer le formulaire  ">
 	</center>
  </div>
 </form>
@@ -72,8 +66,8 @@
 		<?php include("footer.php"); ?>
 </footer>
 </div>
- <div id="bg"></div> 
-   
+ <div id="bg"></div>
+
     <script src="assets/js/jquery.min.js"></script>
       <script src="assets/js/browser.min.js"></script>
       <script src="assets/js/breakpoints.min.js"></script>
