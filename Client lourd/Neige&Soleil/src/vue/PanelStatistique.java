748b0e117ed98630ede104f0d7ca670d6786bf9d
ayoub <aybinho@github.com>
2019-04-19 16:13:24 +0200

    Update PanelStatistique.java

--- Client lourd/Neige&Soleil/src/vue/PanelStatistique.java
+++ Client lourd/Neige&Soleil/src/vue/PanelStatistique.java
@@ -1,16 +1,7 @@
 package vue;
 
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
